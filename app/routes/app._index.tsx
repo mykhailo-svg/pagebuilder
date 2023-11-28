236b918f1899b3af59b1bb087d72974ef83ce7b4
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-11-29 01:43:10 +1200

    Init project

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,10 @@
+import { Page, Layout } from "@shopify/polaris";
+
+export default function Index() {
+  return (
+    <Page>
+      <ui-title-bar title="Main page"></ui-title-bar>
+      <Layout></Layout>
+    </Page>
+  );
+}
---
+++ app/routes/app.additional.tsx
@@ -0,0 +1,10 @@
+import { Layout, Page } from "@shopify/polaris";
+
+export default function AdditionalPage() {
+  return (
+    <Page>
+      <ui-title-bar title="Additional page" />
+      <Layout></Layout>
+    </Page>
+  );
+}
