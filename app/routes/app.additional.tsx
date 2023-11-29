6e7da0c4f3725c93c1d884e5220660ca5895d4ea
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-11-29 16:08:46 +1200

    Add basic string to html parsing

--- app/routes/app.additional.tsx
+++ app/routes/app.additional.tsx
@@ -1,10 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import { Layout, Page } from '@shopify/polaris';
 
 export default function AdditionalPage() {
+  const [markdown, setMarkdown] = useState<string>('<div>Hii</div>');
+
+  useEffect(() => {
+    // Перевірка, чи код виконується на клієнтській стороні
+    if (typeof window !== 'undefined') {
+      console.log(new DOMParser().parseFromString(markdown, 'text/html'));
+    }
+  }, [markdown]);
+
   return (
     <Page>
-      <ui-title-bar title="Additional page" />
-      <Layout></Layout>
+      <ui-title-bar title="Page Builder" />
+      <Layout>
+        <div dangerouslySetInnerHTML={{ __html: markdown }} />
+      </Layout>
     </Page>
   );
 }
