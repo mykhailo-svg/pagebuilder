ab4d084ee3f512f77464e599d2bfabc2d3cbcc89
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-02 00:56:46 +1200

    Starting implementing markdown updating form

--- app/routes/app.additional.tsx
+++ app/routes/app.additional.tsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
-import { Button, Page } from '@shopify/polaris';
+import { Button, Form, Page } from '@shopify/polaris';
 import grapesjs, { Editor } from 'grapesjs';
 import gjsPresetWebpage from 'grapesjs-preset-webpage';
 import gjsPluginBlocksBasic from 'grapesjs-blocks-basic';
 import gjsPluginCkEditor from 'grapesjs-plugin-ckeditor';
 import grapesStyles from 'grapesjs/dist/css/grapes.min.css';
 import { parseIntoLiquid } from '~/helpers/parseIntoLiquid';
-import { LoaderFunction, json } from '@remix-run/node';
+import {
+  ActionFunctionArgs,
+  LoaderFunction,
+  json,
+  redirect,
+} from '@remix-run/node';
 import { authenticate } from '~/shopify.server';
 import { createNewPage, getPageById } from '~/models/page.server';
-import { useLoaderData } from '@remix-run/react';
+import { useActionData, useLoaderData } from '@remix-run/react';
+
 export const links = () => [{ rel: 'stylesheet', href: grapesStyles }];
 
 type PageType = {
@@ -38,9 +44,25 @@ export const loader: LoaderFunction = async ({ request }) => {
   }
 };
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+
+  const formDataObject: Record<string, string> = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value.toString();
+  });
+
+  return json({
+    form: formDataObject,
+  });
+};
+
 export default function AdditionalPage() {
   const [editor, setEditor] = useState<Editor>();
+  const [formData, setFormData] = useState<Record<string, string>>({}); // Додавання стейту для даних форми
   const response = useLoaderData<LoaderResponse>();
+  const updatePageResponse = useActionData<typeof action>();
+  console.log(updatePageResponse + 'sd');
 
   useEffect(() => {
     const editor = grapesjs.init({
@@ -56,18 +78,26 @@ export default function AdditionalPage() {
     setEditor(editor);
   }, []);
 
+  const handleSubmit = () => {
+    const html = editor?.getHtml() ?? '';
+    const css = editor?.getCss() ?? '';
+    console.log(parseIntoLiquid({ html, css }));
+
+    // Оновлення стейту з даними форми
+    const newFormData = { ...formData, html, css };
+    setFormData(newFormData);
+  };
+
   return (
     <Page fullWidth>
-      <Button
-        onClick={() => {
-          const html = editor?.getHtml() ?? '';
-          const css = editor?.getCss() ?? '';
-          console.log(parseIntoLiquid({ html, css }));
-        }}
-      >
-        Export
-      </Button>
-      <div id="editor"></div>
+      <Form method="post" onSubmit={handleSubmit}>
+        <Button submit>Export</Button>
+        <div id="editor"></div>
+        <div>
+          Submitted Form Data:
+          <pre>{JSON.stringify(updatePageResponse?.form, null, 2)}</pre>
+        </div>
+      </Form>
     </Page>
   );
 }
