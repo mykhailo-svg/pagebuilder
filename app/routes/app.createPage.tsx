49b7e327be244dac268e275b4ccd7aa4e4abcf3e
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-01 17:22:45 +1200

    Fetching themes

--- app/routes/app.createPage.tsx
+++ app/routes/app.createPage.tsx
@@ -9,13 +9,20 @@ type Shop = {
   id: string;
 };
 
+type Theme = {
+  id: number;
+  name: string;
+  role: string;
+};
+
 type InitialResponse = {
   shop: Shop;
+  themes: Theme[];
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
   try {
-    const { admin } = await authenticate.admin(request);
+    const { admin, session } = await authenticate.admin(request);
 
     const getProducts = `#graphql
       {
@@ -27,9 +34,12 @@ export const loader: LoaderFunction = async ({ request }) => {
     `;
 
     const response = await admin.graphql(getProducts);
+    const themesResponse = await admin.rest.resources.Theme.all({
+      session: session,
+    });
     const data = await response.json();
 
-    return json(data.data);
+    return json({ ...data.data, themes: themesResponse.data });
   } catch (error) {
     console.error('Error fetching data:', error);
 
@@ -39,13 +49,15 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function createPage() {
   const [shop, setShop] = useState<Shop>({ id: '', name: '' });
+  const [themes, setThemes] = useState<Theme[]>([]);
 
   const response = useLoaderData<InitialResponse>();
 
   useEffect(() => {
     setShop(response.shop);
+    setThemes(response.themes);
   }, []);
-  console.log(shop);
+  console.log(themes);
 
   return (
     <Page fullWidth>
