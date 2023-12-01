58779c05249422ec23d6322003a36d730f95c249
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-01 17:44:20 +1200

    Add themse picker component

--- app/routes/app.createPage.tsx
+++ app/routes/app.createPage.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Button, Page } from '@shopify/polaris';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, ChoiceList, Page, TextField } from '@shopify/polaris';
 import { authenticate } from '~/shopify.server';
 import { LoaderFunction, json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
@@ -47,12 +47,23 @@ export const loader: LoaderFunction = async ({ request }) => {
   }
 };
 
+const PickerChildren = () => {
+  return <div>wefdgd</div>;
+};
+
 export default function createPage() {
   const [shop, setShop] = useState<Shop>({ id: '', name: '' });
   const [themes, setThemes] = useState<Theme[]>([]);
+  const choices = [{ label: 'None', value: 'none' }];
 
   const response = useLoaderData<InitialResponse>();
+  const [selected, setSelected] = useState<string[]>(['']);
+  const [textFieldValue, setTextFieldValue] = useState('');
 
+  const handleChange = useCallback((value: string[]) => setSelected(value), []);
+  const renderChildren = () => <PickerChildren />;
+
+  const renderPickerChildren = (ad: string) => <PickerChildren />;
   useEffect(() => {
     setShop(response.shop);
     setThemes(response.themes);
@@ -62,7 +73,16 @@ export default function createPage() {
   return (
     <Page fullWidth>
       <ui-title-bar title={`Shop: ${shop.name}`}></ui-title-bar>
-      <Button> </Button>
+      {themes ? (
+        <ChoiceList
+          title="Discount minimum requirements"
+          choices={choices}
+          selected={selected}
+          onChange={handleChange}
+        />
+      ) : (
+        ''
+      )}
     </Page>
   );
 }
