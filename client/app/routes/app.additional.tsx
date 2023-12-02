48b6b72540a3881d5851abf09f45dd5e2d59033a
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-03 02:51:22 +1200

    Starting adding custom editor

--- client/app/routes/app.additional.tsx
+++ client/app/routes/app.additional.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { useState, useEffect } from 'react';
 import { Button, Page } from '@shopify/polaris';
 import type { Editor } from 'grapesjs';
@@ -100,7 +101,116 @@ export default function AdditionalPage() {
   return (
     <Page fullWidth>
       <Button onClick={handleSubmit}>Export</Button>
-      <div id="editor"></div>
+      <div id="navbar" className="sidenav d-flex flex-column overflow-scroll">
+        <nav className="navbar navbar-light">
+          <div className="container-fluid">
+            <span className="navbar-brand mb-0 h3 logo">Code Dexterous</span>
+          </div>
+        </nav>
+        <div className="">
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link active"
+                id="block-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#block"
+                type="button"
+                role="tab"
+                aria-controls="block"
+                aria-selected="true"
+              >
+                <i className="fa fa-cubes"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="layer-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#layer"
+                type="button"
+                role="tab"
+                aria-controls="layer"
+                aria-selected="false"
+              >
+                <i className="fa fa-tasks"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="style-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#style"
+                type="button"
+                role="tab"
+                aria-controls="style"
+                aria-selected="false"
+              >
+                <i className="fa fa-paint-brush"></i>
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="nav-link"
+                id="trait-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#trait"
+                type="button"
+                role="tab"
+                aria-controls="trait"
+                aria-selected="false"
+              >
+                <i className="fa fa-cog"></i>
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div
+              className="tab-pane fade show active"
+              id="block"
+              role="tabpanel"
+              aria-labelledby="block-tab"
+            >
+              <div id="blocks"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="layer"
+              role="tabpanel"
+              aria-labelledby="layer-tab"
+            >
+              <div id="layers-container"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="style"
+              role="tabpanel"
+              aria-labelledby="style-tab"
+            >
+              <div id="styles-container"></div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="trait"
+              role="tabpanel"
+              aria-labelledby="trait-tab"
+            >
+              <div id="trait-container"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main-content">
+        <nav className="navbar navbar-light">
+          <div className="container-fluid">
+            <div className="panel__devices"></div>
+            <div className="panel__basic-actions"></div>
+          </div>
+        </nav>
+        <div id="editor"></div>
+      </div>
     </Page>
   );
 }
