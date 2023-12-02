4619a193ae160f6c9c8ad9677989859daf3e2df5
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-02 18:38:25 +1200

    Add resource list

---
+++ client/app/routes/app.pages.tsx
@@ -0,0 +1,50 @@
+import {
+  LegacyCard,
+  ResourceList,
+  Avatar,
+  ResourceItem,
+  Text,
+} from '@shopify/polaris';
+import React from 'react';
+
+export default function Pages() {
+  return (
+    <LegacyCard>
+      <ResourceList
+        resourceName={{ singular: 'customer', plural: 'customers' }}
+        items={[
+          {
+            id: '100',
+            url: '#',
+            name: 'Mae Jemison',
+            location: 'Decatur, USA',
+          },
+          {
+            id: '200',
+            url: '#',
+            name: 'Ellen Ochoa',
+            location: 'Los Angeles, USA',
+          },
+        ]}
+        renderItem={(item) => {
+          const { id, url, name, location } = item;
+          const media = <Avatar customer size="md" name={name} />;
+
+          return (
+            <ResourceItem
+              id={id}
+              url={url}
+              media={media}
+              accessibilityLabel={`View details for ${name}`}
+            >
+              <Text variant="bodyMd" fontWeight="bold" as="h3">
+                {name}
+              </Text>
+              <div>{location}</div>
+            </ResourceItem>
+          );
+        }}
+      />
+    </LegacyCard>
+  );
+}
