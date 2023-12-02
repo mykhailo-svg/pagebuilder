ed5b4f3b1f0c01a716f87090c3a2d014d8f963d2
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-02 16:47:54 +1200

    Add page creating

--- server/src/controllers/page.controller.js
+++ server/src/controllers/page.controller.js
@@ -1,7 +1,15 @@
+const Page = require('../models/page.model');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const httpStatus = require('http-status');
 
-const createPage = catchAsync(async (req, res) => {});
+const createPage = catchAsync(async (req, res) => {
+  const options = {
+    themeId: req.body.themeId,
+    shop: req.body.shop,
+  };
+  const page = await Page.create(options);
+  res.status(httpStatus.OK).send(page);
+});
 
 module.exports = { createPage };
--- server/src/models/page.model.js
+++ server/src/models/page.model.js
@@ -6,7 +6,9 @@ const pageSchema = mongoose.Schema(
     themeId: { type: String, required: true },
     html: { type: String, default: 'none' },
     css: { type: String, default: 'none' },
-    shop: { type: String, required: true }
+    shop: { type: String, required: true },
+    isInShopify: { type: Boolean, default: false },
+    isPublished: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
