867f74a75f89378b14ee409eae36a71c91dd0f8e
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-02 18:58:19 +1200

    fetch pages

--- client/app/routes/app.pages.tsx
+++ client/app/routes/app.pages.tsx
@@ -1,3 +1,5 @@
+import { LoaderFunction, json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import {
   LegacyCard,
   ResourceList,
@@ -5,8 +7,52 @@ import {
   ResourceItem,
   Text,
 } from '@shopify/polaris';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { authenticate } from '~/shopify.server';
+type PageType = {
+  id: string;
+  css: string;
+  html: string;
+  themeId: string;
+  shop: string;
+  isPublished: boolean;
+  isInShopify: boolean;
+};
+
+type InitialLoaderResponse = {
+  pagesData: PageType[];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  try {
+    const { admin } = await authenticate.admin(request);
+    const url = new URL(request.url);
+    const pageId = url.searchParams.get('pageId') || '';
+    const pages = await axios
+      .get(`http://localhost:4000/v1/page/created/test2r3`)
+      .catch((error) => {
+        console.error('Помилка відправлення POST-запиту:', error);
+      });
+    const pagesData = pages ? pages.data : null;
+
+    return json({ pagesData });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+
+    return json([]);
+  }
+};
 
 export default function Pages() {
+  const [pages, setPages] = useState<PageType[]>([]);
+  const response = useLoaderData<InitialLoaderResponse>();
+  console.log(pages);
+
+  useEffect(() => {
+    setPages(response.pagesData);
+  }, []);
+
   return (
     <LegacyCard>
       <ResourceList
@@ -39,7 +85,7 @@ export default function Pages() {
               <Text variant="bodyMd" fontWeight="bold" as="h3">
                 {name}
               </Text>
-              <div>{location}</div>
+              <div>{'sdds'}</div>
             </ResourceItem>
           );
         }}
