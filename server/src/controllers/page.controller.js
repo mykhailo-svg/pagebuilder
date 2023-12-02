b98ae5784372a7644b0cc31f79ad458f3554a651
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-02 16:58:40 +1200

    Add page getting by id and updating

--- server/src/controllers/page.controller.js
+++ server/src/controllers/page.controller.js
@@ -6,10 +6,30 @@ const httpStatus = require('http-status');
 const createPage = catchAsync(async (req, res) => {
   const options = {
     themeId: req.body.themeId,
-    shop: req.body.shop,
+    shop: req.body.shop
   };
   const page = await Page.create(options);
   res.status(httpStatus.OK).send(page);
 });
 
-module.exports = { createPage };
+const getPageById = catchAsync(async (req, res) => {
+  const page = await Page.findById(req.params.pageId);
+  res.status(httpStatus.OK).send(page);
+});
+
+const updatePage = catchAsync(async (req, res) => {
+  if (!req.body.html || !req.body.css) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Set html and css');
+  }
+  const page = await Page.findByIdAndUpdate(
+    req.params.pageId,
+    {
+      css: req.body.css,
+      html: req.body.html
+    },
+    { new: true }
+  );
+  res.status(httpStatus.OK).send(page);
+});
+
+module.exports = { createPage, getPageById, updatePage };
--- server/src/routes/v1/page.route.js
+++ server/src/routes/v1/page.route.js
@@ -4,5 +4,6 @@ const { pageController } = require('../../controllers');
 const router = express.Router();
 
 router.route('/').post(pageController.createPage);
+router.route('/:pageId').get(pageController.getPageById).put(pageController.updatePage);
 
 module.exports = router;
