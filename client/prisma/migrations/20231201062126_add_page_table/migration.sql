17a3c2a6a2db0bbc5b3e376bb63f67e4984720da
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-05 17:41:52 +1200

    Improve page schema

--- client/prisma/migrations/20231201062126_add_page_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Page" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "css" TEXT NOT NULL DEFAULT 'none',
-    "html" TEXT NOT NULL DEFAULT 'none',
-    "themeId" TEXT NOT NULL
-);
