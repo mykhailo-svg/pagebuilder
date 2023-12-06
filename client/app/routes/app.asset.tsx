90afcc5c3f25806ce98fa80cba2d70cf50b812e6
aleksandrchuyko <chao.mess@gmail.com>
2023-12-06 17:52:29 +0200

    product

--- client/app/routes/app.asset.tsx
+++ client/app/routes/app.asset.tsx
@@ -3,7 +3,12 @@ import { json } from '@remix-run/node';
 import { Form, useActionData } from '@remix-run/react';
 import { Page, Layout, Button } from '@shopify/polaris';
 import { authenticate } from '~/shopify.server';
+
+import resource from '../constants/product.json' assert { type: 'json' };
+
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const name = 'custom';
+  const newResource: any = resource;
   const { admin, session } = await authenticate.admin(request);
   const asset = new admin.rest.resources.Asset({ session: session });
   asset.theme_id = 163021127986;
@@ -11,12 +16,25 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   // 163021127986;
   // 131827335324;
 
-  asset.key = 'sections/a2.liquid';
+  asset.key = `sections/${name}.liquid`;
   asset.value =
     "<div><img src='backsoon-postit.png'><p>We are busy updating the store for you and will be back within the hour.</p></div>";
   await asset.save();
+
+  const tpl = new admin.rest.resources.Asset({ session: session });
+  tpl.theme_id = 163021127986;
+
+  tpl.key = `templates/product.${name}.json`;
+  newResource.sections[name] = {
+    type: name,
+    settings: {},
+  };
+  newResource.order.push(name);
+  tpl.value = JSON.stringify(resource);
+  await tpl.save();
   return json({
     asset,
+    tpl,
   });
 };
 
