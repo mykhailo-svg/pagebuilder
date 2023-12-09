ad943fee269c85e3f4959ce2440e58c0a8689bdd
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-09 17:20:31 +1200

    Rvieve playgroung

--- client/app/routes/app.asset.tsx
+++ client/app/routes/app.asset.tsx
@@ -1,37 +1,33 @@
 import type { ActionFunctionArgs } from '@remix-run/node';
-import { json, redirect } from '@remix-run/node';
-import { Form, useActionData, useSubmit } from '@remix-run/react';
-import { useRef } from 'react';
+import { json } from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
+import { Page, Layout, Button } from '@shopify/polaris';
+import { authenticate } from '~/shopify.server';
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { admin, session } = await authenticate.admin(request);
+  const asset = new admin.rest.resources.Asset({ session: session });
+  asset.theme_id = 131827335324;
+  asset.key = 'templates/olol.liquid';
+  asset.value =
+    "<img src='backsoon-postit.png'><p>We are busy updating the store for you and will be back within the hour.</p>";
+  await asset.save();
+  return json({
+    asset,
+  });
+};
 
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const arbitraryData = formData.get('arbitraryData');
-  const title = formData.get('title');
-
-  return json({ arbitraryData, title });
-}
-
-export default function Todos() {
-  console.log(useActionData());
-
-  const submit = useSubmit();
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-
-    const formData = new FormData(formRef.current as HTMLFormElement);
-    formData.append('arbitraryData', 'sd1');
-
-    submit(formData, { method: 'post', action: '/app/asset' });
-  };
+export default function Index() {
+  const response = useActionData();
+  console.log(response);
 
   return (
-    <div>
-      <Form ref={formRef} method="post" onSubmit={handleSubmit}>
-        <input type="text" name="title" />
-        <button type="submit">Create Todo</button>
-      </Form>
-    </div>
+    <Page>
+      <ui-title-bar title="Main page"></ui-title-bar>
+      <Layout>
+        <Form method="put">
+          <Button submit>Submit</Button>
+        </Form>
+      </Layout>
+    </Page>
   );
 }
