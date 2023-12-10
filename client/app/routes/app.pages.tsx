34ccb95f3274d8768568bb5b673a24af63d08445
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-10 19:04:42 +1200

    Add basic table

--- client/app/routes/app.pages.tsx
+++ client/app/routes/app.pages.tsx
@@ -4,13 +4,17 @@ import { useLoaderData } from '@remix-run/react';
 import {
   ResourceList,
   ResourceItem,
+  IndexTable,
   Text,
   Badge,
   Card,
   Page,
   EmptyState,
+  useIndexResourceState,
   InlineGrid,
   ResourceListProps,
+  Link,
+  Button,
 } from '@shopify/polaris';
 import { useState } from 'react';
 import type { PageType } from '~/global_types';
@@ -56,13 +60,79 @@ export default function Pages() {
     },
   ];
 
-  const promotedBulkActions = [
+  const orders = [
     {
-      content: 'Edit customers',
-      onAction: () => console.log('Todo: implement bulk edit'),
+      id: '1020',
+      order: '#1020',
+      date: 'Jul 20 at 4:34pm',
+      customer: 'Jaydon Stanton',
+      total: '$969.44',
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: '1019',
+      order: '#1019',
+      date: 'Jul 20 at 3:46pm',
+      customer: 'Ruben Westerfelt',
+      total: '$701.19',
+      paymentStatus: <Badge progress="partiallyComplete">Partially paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: '1018',
+      order: '#1018',
+      date: 'Jul 20 at 3.44pm',
+      customer: 'Leo Carder',
+      total: '$798.24',
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
     },
   ];
 
+  const { selectedResources, allResourcesSelected, handleSelectionChange } =
+    useIndexResourceState(orders);
+
+  const rowMarkup = orders.map(
+    (
+      { id, order, date, customer, total, paymentStatus, fulfillmentStatus },
+      index
+    ) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <Link url="/app/" dataPrimaryLink>
+            <Text variant="bodyMd" fontWeight="bold" as="span">
+              {order}
+            </Text>
+          </Link>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>{total}</IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    )
+  );
+  const promotedBulkActions = [
+    {
+      content: 'Create shipping labels',
+      onAction: () => console.log('Todo: implement create shipping labels'),
+    },
+    {
+      content: 'Mark as fulfilled',
+      onAction: () => console.log('Todo: implement mark as fulfilled'),
+    },
+    {
+      content: 'Capture payment',
+      onAction: () => console.log('Todo: implement capture payment'),
+    },
+  ];
   const bulkActions = [
     {
       content: 'Add tags',
@@ -77,45 +147,65 @@ export default function Pages() {
       onAction: () => console.log('Todo: implement bulk delete'),
     },
   ];
-
   return (
     <Page>
       <ui-title-bar title="Pages"></ui-title-bar>
       <Card>
         {response.length ? (
-          <ResourceList
-            resourceName={{ singular: 'customer', plural: 'customers' }}
-            items={response}
-            selectedItems={selectedItems}
-            onSelectionChange={setSelectedItems}
-            selectable
-            renderItem={(item) => {
-              const { id, name, status, templateType } = item;
+          // <ResourceList
+          //   resourceName={{ singular: 'customer', plural: 'customers' }}
+          //   items={response}
+          //   selectedItems={selectedItems}
+          //   onSelectionChange={setSelectedItems}
+          //   selectable
+          //   renderItem={(item) => {
+          //     const { id, name, status, templateType } = item;
 
-              return (
-                <ResourceItem
-                  id={id}
-                  url={`/app/editor?pageId=${id}`}
-                  accessibilityLabel={`View details for ${name}`}
-                >
-                  <InlineGrid columns={3}>
-                    <Text variant="bodyMd" fontWeight="bold" as="h3">
-                      {name}
-                    </Text>
-                    <Text variant="bodyMd" fontWeight="bold" as="h3">
-                      {templateType}
-                    </Text>
-                    <Badge
-                      tone={definePageBadgesStatus(status).tone}
-                      progress={definePageBadgesStatus(status).progress}
-                    >
-                      {definePageBadgesStatus(status).text}
-                    </Badge>
-                  </InlineGrid>
-                </ResourceItem>
-              );
-            }}
-          />
+          //     return (
+          //       <ResourceItem
+          //         id={id}
+          //         url={`/app/editor?pageId=${id}`}
+          //         accessibilityLabel={`View details for ${name}`}
+          //       >
+          //         <InlineGrid columns={3}>
+          //           <Text variant="bodyMd" fontWeight="bold" as="h3">
+          //             {name}
+          //           </Text>
+          //           <Text variant="bodyMd" fontWeight="bold" as="h3">
+          //             {templateType}
+          //           </Text>
+          //           <Badge
+          //             tone={definePageBadgesStatus(status).tone}
+          //             progress={definePageBadgesStatus(status).progress}
+          //           >
+          //             {definePageBadgesStatus(status).text}
+          //           </Badge>
+          //         </InlineGrid>
+          //       </ResourceItem>
+          //     );
+          //   }}
+          // />
+          <IndexTable
+            resourceName={resourceName}
+            itemCount={orders.length}
+            selectedItemsCount={
+              allResourcesSelected ? 'All' : selectedResources.length
+            }
+            onSelectionChange={handleSelectionChange}
+            hasMoreItems
+            bulkActions={bulkActions}
+            promotedBulkActions={promotedBulkActions}
+            headings={[
+              { title: 'Order' },
+              { title: 'Date' },
+              { title: 'Customer' },
+              { title: 'Total', alignment: 'end' },
+              { title: 'Payment status' },
+              { title: 'Fulfillment status' },
+            ]}
+          >
+            {rowMarkup}
+          </IndexTable>
         ) : (
           <EmptyState
             heading="You don't have created pages"
