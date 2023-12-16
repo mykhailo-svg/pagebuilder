bab5e0c2f9211c67ada293496e9edd05210fddf8
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-17 05:25:24 +1200

    Solve propblems

--- client/app/routes/app.asset.tsx
+++ client/app/routes/app.asset.tsx
@@ -1,49 +1,97 @@
-import type { ActionFunctionArgs } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { Form, useActionData } from '@remix-run/react';
-import { Page, Layout, Button } from '@shopify/polaris';
-import { Fullscreen } from '@shopify/app-bridge/actions';
-import { authenticate } from '~/shopify.server';
-import { useAppBridge } from '@shopify/app-bridge-react';
-import { useEffect } from 'react';
-export const action = async ({ request }: ActionFunctionArgs) => {
-  const { admin, session } = await authenticate.admin(request);
-  const asset = new admin.rest.resources.Asset({ session: session });
-  asset.theme_id = 131827335324;
-  asset.key = 'templates/olol.liquid';
-  asset.value =
-    "<img src='backsoon-postit.png'><p>We are busy updating the store for you and will be back within the hour.</p>";
-  await asset.save();
-  return json({
-    asset,
-  });
+//@ts-nocheck
+import * as React from 'react';
+import GjsEditor, {
+  AssetsProvider,
+  Canvas,
+  ModalProvider,
+} from '@grapesjs/react';
+import type { Editor, EditorConfig } from 'grapesjs';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { MAIN_BORDER_COLOR } from '../components/common';
+import CustomModal from '../components/CustomModal';
+import CustomAssetManager from '../components/CustomAssetManager';
+import Topbar from '../components/Topbar';
+import RightSidebar from '../components/RightSidebar';
+import { useEffect, useState } from 'react';
+
+const gjsOptions: EditorConfig = {
+  height: '100vh',
+  storageManager: false,
+  undoManager: { trackSelection: false },
+  selectorManager: { componentFirst: true },
+  projectData: {
+    assets: [
+      'https://via.placeholder.com/350x250/78c5d6/fff',
+      'https://via.placeholder.com/350x250/459ba8/fff',
+      'https://via.placeholder.com/350x250/79c267/fff',
+      'https://via.placeholder.com/350x250/c5d647/fff',
+      'https://via.placeholder.com/350x250/f28c33/fff',
+    ],
+    pages: [
+      {
+        name: 'Home page',
+        component: `<h1>GrapesJS React Custom UI</h1>`,
+      },
+    ],
+  },
 };
 
-export default function Index() {
-  const response = useActionData();
-  console.log(response);
-  const app = useAppBridge();
+export default function App() {
+  const [GjsEditor, setGjsEditor] = useState<any>(null);
+
   useEffect(() => {
-    if (app) {
-      const fullscreen = Fullscreen.create(app);
-      fullscreen.dispatch(Fullscreen.Action.ENTER);
-    }
-  }, [app]);
-  return (
-    <Page>
-      <ui-title-bar title="Main page"></ui-title-bar>
-      <Layout>
-        <Form method="put">
-          <Button
-            onClick={() => {
-              const fullscreen = Fullscreen.create(app);
-              fullscreen.dispatch(Fullscreen.Action.EXIT);
-            }}
+    const loadGrapesJS = async () => {
+      try {
+        // Dynamically import GrapesJS and its related modules
+        const [
+          Editor,
+          GjsEditorModule,
+
+          // Other modules...
+        ] = await Promise.all([
+          import('grapesjs'),
+          import('@grapesjs/react'),
+
+          // Import other modules dynamically...
+        ]);
+
+        // Set up GrapesJS options and other configurations...
+        const gjsOptions = {
+          // Your GrapesJS options...
+        };
+
+        // Your onEditor function...
+        const onEditor = (editor: any) => {
+          console.log('Editor loaded');
+          (window as any).editor = editor;
+        };
+
+        // Set the GjsEditor component with the dynamically imported module
+        setGjsEditor(() => (
+          <GjsEditorModule.default
+            className="gjs-custom-editor text-white bg-slate-900"
+            grapesjs="https://unpkg.com/grapesjs"
+            grapesjsCss="https://unpkg.com/grapesjs/dist/css/grapes.min.css"
+            options={gjsOptions}
+            plugins={[
+              {
+                id: 'gjs-blocks-basic',
+                src: 'https://unpkg.com/grapesjs-blocks-basic',
+              },
+            ]}
+            onEditor={onEditor}
           >
-            Submit
-          </Button>
-        </Form>
-      </Layout>
-    </Page>
-  );
+            {/* Your JSX structure... */}
+          </GjsEditorModule.default>
+        ));
+      } catch (error) {
+        console.error('Error loading GrapesJS:', error);
+      }
+    };
+
+    // Call the function to load GrapesJS when the component mounts
+    loadGrapesJS();
+  }, []); // Empty dependency array to ensure the effect runs only once
+
+  return <>{GjsEditor}</>;
 }
