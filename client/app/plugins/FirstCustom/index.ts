8fc3245c3bac6be153603d1dd34b0ff7504bf018
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2023-12-19 16:59:35 +0200

    Initing plugin

--- client/app/plugins/FirstCustom/index.ts
+++ client/app/plugins/FirstCustom/index.ts
@@ -1,6 +1,19 @@
 import grapesjs from "grapesjs";
 
-export default grapesjs.plugins.add("customEditorComponent", ((editor, options) => {
+export default grapesjs.plugins.add("customEditorComponent", ((editor, opts) => {
+    let options = {
+        label: "Customcc",
+        name: "CComponent",
+        category: "Custom"
+    };
+
+    // Extend opts with default values from options
+    for (let name in options) {
+        if (!(name in opts)) {
+            opts[name] = options[name as "label"];
+        }
+    }
+
 
 
 }))
