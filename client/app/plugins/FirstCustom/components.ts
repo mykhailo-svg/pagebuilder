c0afc528e15cadd13dd0fe75eb7472b456471a2d
Vyzdryk Mykhailo <wyzdrykm@gmail.com>
2023-12-20 07:14:59 +0200

    Adding traits

--- client/app/plugins/FirstCustom/components.ts
+++ client/app/plugins/FirstCustom/components.ts
@@ -6,6 +6,18 @@ export default (editor: Editor, opts = {} as any) => {
     dc.addType(opts.name, {
         model: {
             defaults: {
+                traits: [
+                    { type: "checkbox", name: "dynamicProgress", label: "Dynamic progress", changeProp: true, }, {
+                        type: "select",
+                        name: "Progress_type",
+                        label: "Progress type",
+                        changeProp: true,
+                        options: [
+                            { value: "bullets", name: "Bullets" },
+                            { value: "fraction", name: "fraction" }
+                        ]
+                    }
+                ],
                 script: function () {
                     alert('sdsd')
                 }
